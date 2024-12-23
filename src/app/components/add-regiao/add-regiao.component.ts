d416c1f9fda9746a6add478d74b5cd8d63ed405c
André Nogueira <afnogueiradev@gmail.com>
2024-12-23 19:58:27 -0300

    final

--- src/app/components/add-regiao/add-regiao.component.ts
+++ src/app/components/add-regiao/add-regiao.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Regiao } from '../../../Regiao';
 
 @Component({
   selector: 'app-add-regiao',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-regiao.component.html',
   styleUrl: './add-regiao.component.css'
 })
@@ -17,6 +17,9 @@ export class AddRegiaoComponent {
   ativa: boolean = false;
   showform: boolean = false;
 
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 
   onSubmit(){
     console.log(this.nome, this.cidade);
